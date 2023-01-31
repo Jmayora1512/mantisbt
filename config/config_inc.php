13bc6e5a2bbed3ad9a0abe0471c6bd5b5476acd1
Jmayora1512 <jorgemayora88@gmail.com>
2023-01-31 18:40:11 +0100

    Se agrega el proyecto

---
+++ config/config_inc.php
@@ -0,0 +1,10 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = '';
+
+$g_default_timezone       = 'Europe/Berlin';
+
+$g_crypto_master_salt     = 'B76FhB6fiLynJ12CXbZaREG7OIRFtCmY7V72tShqUIU=';
